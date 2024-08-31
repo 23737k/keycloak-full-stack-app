3e3cfdcf249958a55af6657355002e84db6e840b
kenti <016mki69@gmail.com>
2024-08-30 23:58:07 -0300

    Keycloak configuration in the backend

---
+++ backend/src/main/java/com/film/app/filmApp/config/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.film.app.filmApp.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig{
+
+  String[] allowedEndpoints = {
+      "/auth/**",
+      "/v2/api-docs",
+      "/v3/api-docs",
+      "/v3/api-docs/**",
+      "/swagger-resources",
+      "/swagger-resources/**",
+      "/configuration/ui",
+      "/configuration/security",
+      "/swagger-ui/**",
+      "/webjars/**",
+      "/swagger-ui.html"
+  };
+
+  @Bean
+  public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+    http
+        .cors(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(req -> req.requestMatchers(allowedEndpoints).permitAll().anyRequest().authenticated())
+        .oauth2ResourceServer(auth -> auth.jwt( token -> token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
+
+    return http.build();
+  }
+}
--- backend/src/main/java/com/film/app/filmApp/controller/FilmController.java
+++ backend/src/main/java/com/film/app/filmApp/controller/FilmController.java
@@ -6,6 +6,7 @@ import com.film.app.filmApp.service.FilmService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
